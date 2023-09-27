45188cedc03b1bc15864fd7e6fb2c69dc8706465
Osia Miguel Clara Reynoso <osiamiguelclarareynoso@gmail.com>
2023-09-27 15:18:24 -0400

    8th subida

--- Hotel/Hotel.Domain/Repository/ICliente.cs
+++ Hotel/Hotel.Domain/Repository/ICliente.cs
@@ -6,7 +6,7 @@ namespace Hotel.Domain.Repository
     public interface ICliente
     {
         public List<Cliente> GetClientes();
-        public Cliente GetClientes(int id);
+        public Cliente GetCliente(int id);
         public void Save(Cliente cliente);
         public void Update(Cliente cliente);
         public void Remove(Cliente cliente);
