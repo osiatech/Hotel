94fa64b2b8d93220a4311112f5374b74a73a1952
Joan Gomez <joangomez0412@gmail.com>
2023-09-30 19:17:34 -0400

    arreglo categoria repository

--- Hotel/Hotel.Domain/Repository/ICategoriaRepository.cs
+++ Hotel/Hotel.Domain/Repository/ICategoriaRepository.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Hotel.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Hotel.Domain.Repository
 {
-    public class ICategoriaRepository
+    public interface ICategoriaRepository
     {
-
+        public List<Categoria> GetCategorias();
+        public Categoria GetCategorias(int id);
+        public void Save(Categoria categoria);
+        public void Update(Categoria categoria);
+        public void Remove(Categoria categoria);
     }
 }
